69ceb09acf6613b87ad8eeb5f2cd83a7f8db0f6a
Marcus Soll <Superschlumpf@web.de>
2015-07-14 23:59:10 +0200

    Compleated GasNet

--- src/network/gasnet.cpp
+++ src/network/gasnet.cpp
@@ -3,8 +3,10 @@
 #include <QtCore/qmath.h>
 #include <QString>
 #include <QDebug>
+#include <math.h>
 
-// GENE ENCODING: x, y, Rp, Rext, Rort, Rn, Rext, Rort, input, recurrent, WhenGas, TypeGas, Rate of gas (1-11), radius, basis index, bias, W
+// GENE ENCODING: x, y, Rp, Rext, Rort, Rn, Rext, Rort, input, recurrent, WhenGas, TypeGas, Rate of gas (1-11), radius, basis index, bias
+//                0  1   2   3      4    5    6     7     8     9             10     12               13        14         15        16
 
 namespace {
 bool calculate_distance(double x_source, double y_source, double x_target, double y_target)
@@ -50,26 +52,78 @@ bool areNodesConnected(double x_source, double y_source, double x_target, double
 
     return angleCone < orientation;
 }
+
+double floatFromGeneInput(int gene_input, int scalar)
+{
+    return (double) gene_input/RAND_MAX * scalar;
 }
 
-GasNet::GasNet(int len_input, int len_output, double area_size) :
+double weight(int gene_input)
+{
+    return (((double) gene_input - RAND_MAX/2.0) * 2.0 / RAND_MAX);
+}
+
+double cut(double d)
+{
+    if(d > 1.0d)
+    {
+        return 1.0d;
+    }
+    else if(d < 0.0d)
+    {
+        return 0.0d;
+    }
+    return d;
+}
+}
+
+GasNet::GasNet(int len_input, int len_output, double area_size, double gas_threshhold, double electric_threshhold) :
     AbstractNeuralNetwork(len_input, len_output),
     _area_size(area_size),
     _network(NULL),
-    _gas_emitting(NULL)
+    _gas_emitting(NULL),
+    _P(),
+    _gas_threshhold(gas_threshhold),
+    _electric_threshhold(electric_threshhold)
 {
     if(area_size <= 0)
     {
         qFatal(QString("FATAL ERROR in %1 %2: Area needs to be greater then 0").arg(__FILE__).arg(__LINE__).toLatin1().data());
     }
+
+    _P.append(-4.0d);
+    _P.append(-2.0d);
+    _P.append(-1.0d);
+    _P.append(-0.5d);
+    _P.append(-0.25d);
+    _P.append(0.0d);
+    _P.append(0.25d);
+    _P.append(0.5d);
+    _P.append(1.0d);
+    _P.append(2.0d);
+    _P.append(4.0d);
 }
 
 GasNet::GasNet() :
     AbstractNeuralNetwork(1,1),
     _area_size(1.0d),
     _network(NULL),
-    _gas_emitting(NULL)
+    _gas_emitting(NULL),
+    _P(),
+    _gas_threshhold(0.0d),
+    _electric_threshhold(0.0d)
 {
+    _P.append(-4.0d);
+    _P.append(-2.0d);
+    _P.append(-1.0d);
+    _P.append(-0.5d);
+    _P.append(-0.25d);
+    _P.append(0.0d);
+    _P.append(0.25d);
+    _P.append(0.5d);
+    _P.append(1.0d);
+    _P.append(2.0d);
+    _P.append(4.0d);
 }
 
 GasNet::~GasNet()
@@ -86,17 +140,17 @@ GasNet::~GasNet()
 
 GenericGene *GasNet::getRandomGene()
 {
-    return new GenericGene(_gene->segments().length(), 16+_gene->segments().length());
+    return new GenericGene(_len_output*2, 16);
 }
 
 AbstractNeuralNetwork *GasNet::createConfigCopy()
 {
-    return new GasNet(_len_input, _len_output, _area_size);
+    return new GasNet(_len_input, _len_output, _area_size, _gas_threshhold, _electric_threshhold);
 }
 
 void GasNet::_initialise()
 {
-    if(_gene->segments()[0].length() != 16+_gene->segments().length())
+    if(_gene->segments()[0].length() != 16)
     {
         qFatal(QString("FATAL ERROR in %1 %2: Wrong gene segment length!").arg(__FILE__).arg(__LINE__).toLatin1().data());
     }
@@ -112,15 +166,66 @@ void GasNet::_initialise()
 
 void GasNet::_processInput(QList<double> input)
 {
+    double gas1[_gene->segments().length()];
+    double gas2[_gene->segments().length()];
+    double k[_gene->segments().length()];
+
     for(int i = 0; i < _gene->segments().length(); ++i)
     {
-        // Calculate emition of gas
+        // Initiation
+        gas1[i] = 0;
+        gas2[i] = 0;
+        k[i] = 0;
     }
 
-    double k[_gene->segments().length()];
     for(int i = 0; i < _gene->segments().length(); ++i)
     {
         // Calculate gas concentration
+        if(_gas_emitting[i] > 0.0d && _gene->segments()[i][gene_TypeGas]%3 != 0)
+        {
+            for(int j = 0; j < _gene->segments().length(); ++j)
+            {
+                double distance = calculate_distance(floatFromGeneInput(_gene->segments()[i][gene_x], _area_size),
+                                                     floatFromGeneInput(_gene->segments()[i][gene_y], _area_size),
+                                                     floatFromGeneInput(_gene->segments()[j][gene_x], _area_size),
+                                                     floatFromGeneInput(_gene->segments()[j][gene_y], _area_size));
+                if(distance > _gene->segments()[i][gene_Gas_radius])
+                {
+                    continue;
+                }
+                double gas_concentration = qExp((-2 * distance)/floatFromGeneInput(_gene->segments()[i][gene_Gas_radius], _area_size) * _gas_emitting[i]);
+                switch (_gene->segments()[i][gene_TypeGas]%3)
+                {
+                case 1:
+                    gas1[j] += gas_concentration;
+                    break;
+
+                case 2:
+                    gas2[j] += gas_concentration;
+                    break;
+
+                default:
+                    qWarning() << "WARNING in " __FILE__ << " " << __LINE__ << ": Unknown gas" << _gene->segments()[i][gene_TypeGas]%3 << "- ignoring";
+                    break;
+                }
+            }
+        }
+    }
+
+    for(int i = 0; i < _gene->segments().length(); ++i)
+    {
+        // Calculate k
+        int basis_index = _gene->segments()[i][gene_basis_index]%_P.length();
+        int index = qFloor(basis_index + gas1[i] * (_P.length() - basis_index) + gas2[i] * basis_index);
+        if(index < 0)
+        {
+            index = 0;
+        }
+        else if(index >= _P.length())
+        {
+            index = _P.length()-1;
+        }
+        k[i] = _P[index];
     }
 
     double *newNetwork = new double[_gene->segments().length()];
@@ -128,10 +233,110 @@ void GasNet::_processInput(QList<double> input)
     for(int i = 0; i < _gene->segments().length(); ++i)
     {
         // Calculate new input
+        double newValue = 0;
+
+        // Connections
+        for(int j = 0; j < _gene->segments().length(); ++j)
+        {
+            if(i == j)
+            {
+                continue;
+            }
+            if(areNodesConnected(floatFromGeneInput(_gene->segments()[i][gene_x], _area_size),
+                                 floatFromGeneInput(_gene->segments()[i][gene_y], _area_size),
+                                 floatFromGeneInput(_gene->segments()[j][gene_x], _area_size),
+                                 floatFromGeneInput(_gene->segments()[j][gene_y], _area_size),
+                                 floatFromGeneInput(_gene->segments()[i][gene_PositivConeRadius], _area_size),
+                                 floatFromGeneInput(_gene->segments()[i][gene_PositivConeExt], 2*M_PI),
+                                 floatFromGeneInput(_gene->segments()[i][gene_PositivConeOrientation], 2*M_PI)))
+            {
+                newValue += _network[j];
+            }
+            if(areNodesConnected(floatFromGeneInput(_gene->segments()[i][gene_x], _area_size),
+                                 floatFromGeneInput(_gene->segments()[i][gene_y], _area_size),
+                                 floatFromGeneInput(_gene->segments()[j][gene_x], _area_size),
+                                 floatFromGeneInput(_gene->segments()[j][gene_y], _area_size),
+                                 floatFromGeneInput(_gene->segments()[i][gene_NegativConeRadius], _area_size),
+                                 floatFromGeneInput(_gene->segments()[i][gene_NegativConeExt], 2*M_PI),
+                                 floatFromGeneInput(_gene->segments()[i][gene_NegativConeOrientation], 2*M_PI)))
+            {
+                newValue -= _network[j] * -1;
+            }
+        }
+
+        // Recurrent connection
+        switch(_gene->segments()[i][gene_recurrent]%3)
+        {
+        case 1:
+            newValue += _network[i];
+            break;
+        case 2:
+            newValue -= _network[i] * -1;
+            break;
+        default:
+            break;
+        }
+
+        // Input
+        if(_gene->segments()[i][gene_input]%(_len_input+1) != 0)
+        {
+            newValue += input[_gene->segments()[i][gene_input]%(_len_input+1)-1];
+        }
+
+        // K
+        newValue *= k[i];
+
+        // Bias
+        newValue += weight(_gene->segments()[i][gene_bias]);
+
+        // tanh
+        newNetwork[i] = tanh(newValue);
     }
 
     delete _network;
     _network = newNetwork;
+
+    for(int i = 0; i < _gene->segments().length(); ++i)
+    {
+        // Calculate emition of gas
+        bool emittingGas = false;
+        switch (_gene->segments()[i][gene_WhenGas]%3)
+        {
+        case 0: // Electric charge
+            if(_network[i] > _electric_threshhold)
+            {
+                emittingGas = true;
+            }
+            break;
+
+        case 1: // Gas1
+            if(gas1[i] > _gas_threshhold)
+            {
+                emittingGas = true;
+            }
+            break;
+
+        case 2: // Gas2
+            if(gas2[i] > _gas_threshhold)
+            {
+                emittingGas = true;
+            }
+            break;
+
+        default:
+            qWarning() << "WARNING in " __FILE__ << " " << __LINE__ << ": Unknown gas circumstances" << _gene->segments()[i][gene_WhenGas]%3 << "- ignoring";
+            break;
+        }
+
+        if(emittingGas)
+        {
+            _gas_emitting[i] = cut((_gas_emitting[i] + 1.0d) / _gene->segments()[i][gene_Rate_of_gas]);
+        }
+        else
+        {
+            _gas_emitting[i] = cut((_gas_emitting[i] - 1.0d) / _gene->segments()[i][gene_Rate_of_gas]);
+        }
+    }
 }
 
 double GasNet::_getNeuronOutput(int i)
--- src/network/gasnet.h
+++ src/network/gasnet.h
@@ -8,7 +8,7 @@
 class QNNSHARED_EXPORT GasNet : public AbstractNeuralNetwork
 {
 public:
-    GasNet(int len_input, int len_output, double area_size = 1.0d);
+    GasNet(int len_input, int len_output, double area_size = 1.0d, double gas_threshhold=0.1d, double electric_threshhold = 0.5d);
     ~GasNet();
 
     GenericGene *getRandomGene();
@@ -16,6 +16,23 @@ public:
 
 protected:
     GasNet();
+
+    enum GasNet_gene_positions {gene_x = 0,
+                                gene_y = 1,
+                                gene_PositivConeRadius = 2,
+                                gene_PositivConeExt = 3,
+                                gene_PositivConeOrientation = 4,
+                                gene_NegativConeRadius = 5,
+                                gene_NegativConeExt = 6,
+                                gene_NegativConeOrientation = 7,
+                                gene_input = 8,
+                                gene_recurrent = 9,
+                                gene_WhenGas = 10,
+                                gene_TypeGas = 11,
+                                gene_Rate_of_gas= 12,
+                                gene_Gas_radius = 13,
+                                gene_basis_index = 14,
+                                gene_bias= 15};
     void _initialise();
     void _processInput(QList<double> input);
     double _getNeuronOutput(int i);
@@ -23,6 +40,9 @@ protected:
     double _area_size;
     double *_network;
     double *_gas_emitting;
+    QList<double> _P;
+    double _gas_threshhold;
+    double _electric_threshhold;
 };
 
 #endif // GASNET_H
