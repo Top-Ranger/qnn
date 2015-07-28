cb8d5545714c28e576a4f79c0277027667fbeb74
Marcus Soll <Superschlumpf@web.de>
2015-07-28 08:10:12 +0200

    Fixed broken guard in GasNet

--- src/network/gasnet.cpp
+++ src/network/gasnet.cpp
@@ -29,7 +29,7 @@ GasNet::GasNet(int len_input, int len_output, config config) :
     {
         qFatal(QString("FATAL ERROR in %1 %2: Area needs to be greater then 0").arg(__FILE__).arg(__LINE__).toLatin1().data());
     }
-    if(_config.min_size < _len_output)
+    if(_config.min_size != -1 && _config.min_size < _len_output)
     {
         qFatal(QString("FATAL ERROR in %1 %2: min_size_network must not be smaller then output lenght!").arg(__FILE__).arg(__LINE__).toLatin1().data());
     }
